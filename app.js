9dcda84771ed83ede62c5ddd66ad9e9688d6773c
Sovandaa <sovanda@Vandas-MacBook-Pro.local>
2025-11-03 16:20:17 +0700

    Update greeting message

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, GitHub Actions!');
+    res.send('Hello, CI/CD!');
 });
 
 module.exports = app;
-
-// "test": "echo \"Error: no test specified\" && exit 1"
